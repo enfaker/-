913b006b5f694a485cbfcd118440a7ba44732eb8
lzh <1569942337@qq.com>
2020-10-06 21:45:55 +0800

    commit

--- src/test/java/com/luo/algorithm/TestSort.java
+++ src/test/java/com/luo/algorithm/TestSort.java
@@ -9,9 +9,23 @@ public class TestSort {
 
 
     @Test
-    public void testBubbleSort(){
-        int[] array = {4, 511, 443, 5, 19, 45, 2};
-        int[] sortArray = MySort.BubbleSort(array);
-        System.out.println(Arrays.toString(sortArray));
+    public void testSort(){
+        int[] array = {4, 51, 43, 5, 19, 45, 2, 12};
+
+        //冒泡排序
+       /* int[] sortArray = MySort.BubbleSort(array);
+        System.out.println(Arrays.toString(sortArray));*/
+
+       //快速排序
+        /*MySort.quickSort(array,0,array.length-1);
+        System.out.println(Arrays.toString(array));*/
+
+        //直接插入排序
+        /*int[] sortArray = MySort.straightInsertSort(array);
+        System.out.println(Arrays.toString(sortArray));*/
+
+        //希尔排序
+        MySort.shellSort(array);
+        System.out.println(Arrays.toString(array));
     }
 }
